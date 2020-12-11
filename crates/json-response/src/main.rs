711988c4de3fe2dd0cc56f593c21c6f0b6415dbd
Christopher Biscardi <chris@christopherbiscardi.com>
2020-12-11 02:25:24 -0800

    more

--- crates/json-response/src/main.rs
+++ crates/json-response/src/main.rs
@@ -1,19 +1,3 @@
-// use netlify_lambda::{lambda, Context};
-// use serde_json::{json, Value};
-
-// type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
-
-// #[lambda]
-// #[tokio::main]
-// // Note that Request is replaced by Value here, allowing us to
-// // accept and access the body value as JSON via serde
-// async fn main(event: Value, _: Context) -> Result<Value, Error> {
-//     dbg!(&event);
-//     let first_name = event["firstName"].as_str().unwrap_or("world");
-//     dbg!(first_name);
-//     Ok(json!({ "message": format!("Hello, {}!", first_name) }))
-// }
-
 use netlify_lambda_http::{
     lambda::{lambda, Context},
     IntoResponse, Request,
@@ -31,8 +15,6 @@ struct Name {
 
 #[lambda(http)]
 #[tokio::main]
-// Note that Request is replaced by Value here, allowing us to
-// accept and access the body value as JSON via serde
 async fn main(event: Request, _: Context) -> Result<impl IntoResponse, Error> {
     let event: Name = serde_json::from_slice(event.body()).unwrap_or(Name {
         first_name: String::from("world"),
